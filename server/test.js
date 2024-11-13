47b70e31ae41c08e5b4426d9e307edd66d2a33fb
Amogh <53092887+typicallhavok@users.noreply.github.com>
2024-11-13 22:50:55 +0530

    Delete server/test.js

--- server/test.js
+++
@@ -1,8 +0,0 @@
-const { insertNiftyCache, insertCache } = require("./mongo");
-const yahooFinance = require("yahoo-finance2").default;
-const fs = require("fs");
-
-yahooFinance.quote("RELIANCE.NS").then((result) => {
-    fs.writeFileSync("quotes.json", JSON.stringify(result));
-    console.log("done");
-});
